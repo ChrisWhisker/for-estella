2a5da0ae50f108481b48fd38d8b41c0012adb85a
Chris Worcester <chrisworc@gmail.com>
2021-04-11 16:16:53 -0400

    Initial commit

---
+++ Source/Gardening/GardeningGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GardeningGameMode.h"
+#include "GardeningCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AGardeningGameMode::AGardeningGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
