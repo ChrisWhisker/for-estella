83fce9bdc5d6f78767fde8f982a025b623a4c8d1
Chris Worcester <chrisworc@gmail.com>
2021-04-17 13:50:23 -0400

    Revert "Rearrange access modifiers"
    This reverts commit 66646b011882b1e9ed4c49c5c2002b349f7a3503.

--- Source/Gardening/Pawns/GardeningCharacter.h
+++ Source/Gardening/Pawns/GardeningCharacter.h
@@ -30,11 +30,6 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
 	float BaseLookUpRate;
 
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
-
 protected:
 
 	/** Resets HMD orientation in VR. */
@@ -73,6 +68,12 @@ protected:
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	// End of APawn interface
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 
 private:
 	UPROPERTY(VisibleAnywhere, Category=Components)
